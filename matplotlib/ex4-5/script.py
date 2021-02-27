8b0c8ec3779b26f8666322b6595790cf295e3b36
Antoine <antoninus.antoninus@me.com>
2021-02-27 18:19:58 +0100

    [MATPLOTLIB] Exerice 4 et 5

---
+++ matplotlib/ex4-5/script.py
@@ -0,0 +1,22 @@
+# Importez les données de data1.csv dans un dataframe et
+# affichez leur dispersion selon les axes age et prix du billet.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/data1.csv')
+
+plt.subplot(211)
+plt.scatter(df['age'], df['fare'])
+plt.xlabel('age')
+plt.ylabel('tarif')
+
+plt.subplot(212)
+plt.scatter(df['age'][df['survived'] == 1], df['fare'][df['survived'] == 1], c='green', label='survived')
+plt.scatter(df['age'][df['survived'] == 0], df['fare'][df['survived'] == 0], c='yellow', label='no')
+plt.xlabel('age')
+plt.ylabel('tarif')
+
+plt.tight_layout()
+plt.legend()
+plt.show()

2f0ea2d34b2e21cc3e2366cfab5596aa36150fcd
Antoine <antoninus.antoninus@me.com>
2021-02-27 17:04:46 +0100

    [MATPLOTLIB] Exerice 1

---
+++ matplotlib/ex1/script.py
@@ -0,0 +1,14 @@
+# Affichez les fonctions x -> exp(-λx) pour diverses valeurs du
+# paramètre λ. N’oubliez pas l’axe et la légende.
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+lambda_val = [1,5,10]
+x = np.linspace(0, 1)
+
+for val in lambda_val:
+    plt.plot(np.exp(-val * x), label=f"lambda: {val}")
+
+plt.legend()
+plt.show()

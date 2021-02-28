a20a8c88b4aa3e929bfcbb5fe268fdae66decd8f
Antoine <antoninus.antoninus@me.com>
2021-02-28 17:11:24 +0100

    [SKLEARN] Exercice 2

--- scikit-learn/ex2/script.py
+++ scikit-learn/ex2/script.py
@@ -1,5 +1,7 @@
-# Importez les données de data1.csv et testez une régression
-# linéaire entre la longueur et l’épaisseur des pétales.
+# Même question, cette fois entre la longueur des cépales et la
+# largeur des pétales. Comparez les scores des deux
+# régressions.
+# Score: (ici) 0.67 vs 0.93
 
 import pandas as pd
 from os import path
@@ -8,8 +10,8 @@ from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv(path.join(path.dirname(__file__), path.normpath('../../data/scikit-learn/data1.csv')))
 
-X = df[['PetalWidth']].iloc[:, :].values
-y = df[['PetalLength']].iloc[:, :].values
+X = df[['SepalLength']].iloc[:, :].values
+y = df[['PetalWidth']].iloc[:, :].values
 
 reg = LinearRegression()
 reg.fit(X, y)
@@ -22,5 +24,5 @@ plt.scatter(X, y,c='orange')
 plt.plot(X, pred)
 
 plt.xlabel('Epaisseur des Pétales')
-plt.ylabel('Longueur des Pétales')
+plt.ylabel('Longueur des Sépales')
 plt.show()
